e19bd9c6707b09d7ad69046fdc87e2a53afb73f4
Dmitry <dmitry.balduev@gmail.com>
2021-07-17 16:19:23 -0400

    added Me query in resolvers

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -4,7 +4,18 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-      
+      //get a user by username
+      me: async (parent, args, context) => {
+        if(context.user) {
+            const userData = await User.findOne({_id: context.user._id}).populate('savedBooks')
+            return userData;
+        }else{
+            throw new AuthenticationError('Please login first')
+        }
+
+       
+
+     },
     },
   
     Mutation: {
